8ed66452ef0959b52d161c7aae453ac770f06bba
Andrey <andrei.svistun@awtg.co.uk>
2021-05-30 21:46:09 +0300

    add JSDoc for user modules

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -2,9 +2,26 @@ const User = require('./user.model');
 
 let users = [new User()];
 
+/**
+ * @memberOf module:UsersRepo
+ * @returns {Promise<IUser[]>}
+ */
+
 const getAll = async () => users;
+
+/**
+ * @memberOf module:UsersRepo
+ * @param {string} id - ID of user
+ * @returns {Promise<IUserWithoutPassword>}
+ */
 const getById = async (id) => users.find(user => user.id === id);
 
+
+/**
+ * @memberOf module:UsersRepo
+ * @param {IUser} data - New user's data
+ * @returns {Promise<IUserWithoutPassword>}
+ */
 const createUser = async (user) => {
   const newUser = new User(user)
 
@@ -13,12 +30,25 @@ const createUser = async (user) => {
   return newUser;
 }
 
+
+/**
+ * @memberOf module:UsersRepo
+ * @param {string} id - ID of user to delete
+ * @returns {Promise<IUserWithoutPassword>} deleted user
+ */
 const deleteById = async (id) => {
   const findedUser = getById(id);
   users = users.filter(item => item.id !== id)
 
   return findedUser
 }
+
+/**
+ * @memberOf module:UsersRepo
+ * @param {string} id - ID of the user that needs to be updated
+ * @param {Partial<IUser>} data - Optional user properties to update
+ * @returns {Promise<IUserWithoutPassword>} updated user
+ */
 
 const updateById = async (id, user) => {
   const findedUser = users.find(item => item.id === id)
--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,6 +1,21 @@
 const uuid = require('uuid').v4;
 
+/**
+ * @typedef IUserWithoutPassword
+ * @property {string} id - ID of the user
+ * @property {string} login - LOGIN of the user
+ * @property {string} name - NAME of the user
+ */
+
+/**
+ * @typedef {IUserWithoutPassword} IUser
+ * @property {string} password - PASSWORD of the user
+ */
 class User {
+
+  /**
+   * @param {IUser} - User information
+   */
   constructor({
     id = uuid(),
     name = 'USER',
@@ -13,6 +28,11 @@ class User {
     this.password = password;
   }
 
+  /**
+   * @static {function} toResponse -Remove password from User object
+   * @param {IUser} user - User object
+   * @returns {IUserWithoutPassword}
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
