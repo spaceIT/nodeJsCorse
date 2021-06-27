5d3258d8eb9cc918eebd2354a8f097aceaa18da4
Andrey <andrei.svistun@awtg.co.uk>
2021-06-27 22:13:05 +0300

    add settings config, db settings, bcrypt, jsonwebtoken

--- db/db.ts
+++ db/db.ts
@@ -1,5 +1,6 @@
 import { createConnection } from 'typeorm';
 import { DB_CONFIG } from '../src/common/config';
+import { initDB } from './init.db';
 
 const connectToDB = async () => {
     await createConnection(DB_CONFIG);
@@ -9,6 +10,13 @@ const connectToDB = async () => {
 export const tryDBConnect = async (cb: () => void): Promise<void> => {
     try {
         await connectToDB();
+
+        if (await initDB()) {
+            console.log('Default user has created');
+        } else {
+            console.log('Fail to create default user');
+        }
+        
         cb();
     } catch (err) {
         console.log(err);
